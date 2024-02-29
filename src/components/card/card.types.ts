0f33c7bc95474e4480502fccb4c92d1e49f2f0dc
Vlad Denisov <vlad.a.denisov@gmail.com>
2024-02-29 22:38:00 +0300

    2.16. Начинаем программировать

---
+++ src/components/card/card.types.ts
@@ -0,0 +1,10 @@
+export interface CardProps {
+  mark?: string;
+  image: string;
+  price: number;
+  priceText: string;
+  description: string;
+  type: string;
+  rating: number;
+  bookmarked: boolean;
+}
